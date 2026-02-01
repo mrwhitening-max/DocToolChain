c6143762abae81e29108a28d4cd5de5645d9ae64
snozerme <mr.whitening@outlook.de>
2026-02-01 22:38:42 +0100

    Test

--- docToolchainConfig.groovy
+++ docToolchainConfig.groovy
@@ -1,9 +1,22 @@
+// Ausgabepfad für die gebauten Dokumente
+outputPath = 'build'
+
+// Eingabepfad relativ zum Projektverzeichnis
 inputPath = 'src/docs'
 
+// Dokumente und gewünschte Formate
 inputFiles = [
     [file: 'arc42.adoc', formats: ['html', 'pdf']],
 ]
 
+// Verzeichnisse für Bilder
 imageDirs = [
     'images/.',
 ]
+
+// Verzeichnisse und Dateien, die Gradle auf Änderungen überwacht
+taskInputsDirs = [
+    "${inputPath}",
+    "${inputPath}/images",
+]
+taskInputsFiles = []
