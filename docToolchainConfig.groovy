5ec7ee0a22f8cacd0f74d23e0eb6cb35aab613e8
snozerme <mr.whitening@outlook.de>
2026-02-01 22:49:26 +0100

    Arc42 Template added

--- docToolchainConfig.groovy
+++ docToolchainConfig.groovy
@@ -1,22 +1,16 @@
-// Ausgabepfad für die gebauten Dokumente
 outputPath = 'build'
 
-// Eingabepfad relativ zum Projektverzeichnis
 inputPath = 'src/docs'
 
-// Dokumente und gewünschte Formate
 inputFiles = [
     [file: 'arc42.adoc', formats: ['html', 'pdf']],
 ]
 
-// Verzeichnisse für Bilder
 imageDirs = [
     'images/.',
 ]
 
-// Verzeichnisse und Dateien, die Gradle auf Änderungen überwacht
 taskInputsDirs = [
     "${inputPath}",
-    "${inputPath}/images",
 ]
 taskInputsFiles = []
